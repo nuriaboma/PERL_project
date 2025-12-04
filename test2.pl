2dde52765adf13aa92568ffc0afd1780265a8f8d
Núria Bosch Martínez <125981602+nuriaboma@users.noreply.github.com>
2025-12-04 19:03:46 +0100

    Update test2.pl
    no longer stops

--- test2.pl
+++ test2.pl
@@ -19,10 +19,15 @@ my %GRAPH = (
     'BCHILDS'  => []    # bottom childs (terminal nodes)
 );
 
-my ($verbose, $filename, $outputfile, $outfile, $dotformat) = (0, undef, undef, undef, 'png');
-my $K = 21;  # default k-mer size
+my ($verbose, $filename, $outputfile, $outfile, $dotformat, $K) = (0, undef, undef, undef, 'png', 21);
 
-my %VALID_DOTFORMATS = ( 'png'=>0,'jpg'=>0,'gif'=>0,'pdf'=>0 );
+my %VALID_DOTFORMATS = (
+    'png' => 0,
+    'jpg' => 0,
+    'gif' => 0,
+    'pdf' => 0 
+);
+
 
 # COMMAND-LINE OPTIONS
 GetOptions(
@@ -34,15 +39,27 @@ GetOptions(
     "h|help" => \&help
 );
 
+# processing filenames
+
 $filename = shift @ARGV;
 if (defined($outfile)) {
     $outputfile = $outfile ;
 } else {
-    ($outputfile = $filename) =~ s/\.[^\.]+$// if defined $filename;
-}
+    ($outputfile = $filename) =~ s/\.[^\.]+$//;# if defined $filename;
+};
 
 $dotformat = lc($dotformat);
-exists($VALID_DOTFORMATS{$dotformat}) or $dotformat = 'png';
+exists($VALID_DOTFORMATS{$dotformat}) || do {
+    print STDERR "### ERROR ### $dotformat is not a valid graphical format for dot program...\n",
+                 "###           Using default PNG format... (see help information).\n";
+    $dotformat = 'png'; 
+};
+
+# Add a check to ensure kmer is valid
+if ($K <= 1) {
+    die "## ERROR ## You must provide a valid k-mer size greater than 1 using -k (e.g., -k 30).\n";
+}
+
 
 # MAIN
 push @exectime, (new Benchmark);
@@ -50,18 +67,18 @@ print STDERR "##### RUNNING $0 PID[$$] #####\n" if $verbose;
 
 read_from_input_file($verbose, $filename, \%GRAPH);
 
-# optional: skip for very large graphs
-# save_dot_file($verbose, $outputfile, $GRAPH{'P2C'});
-# run_graphviz_dot($verbose, $dotformat, $outputfile);
-
 search_top_to_bottom($verbose, $outputfile."_childs.tbl", $GRAPH{'P2C'}, $GRAPH{'BCHILDS'});
+
 search_top_to_bottom($verbose, $outputfile."_parents.tbl", $GRAPH{'C2P'}, $GRAPH{'TPARENTS'});
 
-traverse_graph_function($verbose, $outputfile."_sentences.tbl", $GRAPH{'P2C'}, $GRAPH{'TPARENTS'});
+traverse_graph_function($verbose, $outputfile."_contigs.tbl", $GRAPH{'P2C'}, $GRAPH{'TPARENTS'});
 
-push @exectime, (new Benchmark);
-print STDERR "##### Time spent: ", timestr(timediff($exectime[-1], $exectime[-2])), " #####\n";
-print STDERR "##### $0 HAS FINISHED #####\n";
+print STDERR "#####\n##### Time spent: ",
+             timestr(timediff($exectime[ $#exectime ],
+                              $exectime[($#exectime - 1) ])),
+             " #####\n#####\n",
+             "##### $0 HAS FINISHED #####\n#####\n"
+             if $verbose;
 
 exit(0);
 
@@ -69,47 +86,61 @@ exit(0);
 
 sub help {
     print STDERR <<'EOHelp';
-USAGE: worderator.pl [options] input_reads.fastq
+USAGE: 
 
-Build a de-Bruijn graph from FASTQ reads using k-mers, list terminal nodes
-and parents, and output the longest contig sequence.
+    worderator.pl [options] input_reads.fastq
 
-Options:
-  -o | --outfile "file.out"      Output filename prefix
-  -t | --dotformat "format"      PNG, JPG, GIF, PDF (default PNG)
-  -k | --kmer <int>              Set k-mer size (default 21)
-  -v | --verbose                 Report execution
-  -d | --debug                   Extended internal debug
-  -h | --help                    Print this help
+DESCRIPTION:
+
+    Build a de-Bruijn graph from FASTQ reads using k-mers, list terminal nodes
+    and parents, and output the longest contig sequence.
+
+OPTIONS:
+
+    -o | --outfile "file.out"      Output filename prefix
+    -t | --dotformat "format"      PNG, JPG, GIF, PDF (default PNG)
+    -k | --kmer <int>              Set k-mer size (default 21)
+    -v | --verbose                 Report execution
+    -d | --debug                   Extended internal debug
+    -h | --help                    Print this help
+
 EOHelp
     exit(1);
 }
 
 sub read_from_input_file {
     my ($verbose, $filename, $graph) = @_;
-    die("## ERROR ## No input file provided!\n") unless defined $filename;
+    die("## ERROR ## No input file provided!\n") unless defined $filename; #No need to try to open file if file was not provided
 
     print STDERR "# Reading FASTQ from $filename, building de-Bruijn graph (k=$K)...\n" if $verbose;
 
-    open(my $IFH, $filename) or die "Cannot open $filename\n";
+    open(IFH, $filename) ||
+        die("## ERROR ## Cannot open $filename...\n");
 
-    my $pairs=0; my $n=0; my $k=$K; my $node_len=$k-1;
-    die("## ERROR ## k must be >=2\n") if $node_len<1;
+    my $pairs=0; 
+    my $n=0; 
+    my $k=$K; 
+    my $node_len=$k-1;
 
     while (1) {
-        my $header = <$IFH>; last unless defined $header;
-        my $seq    = <$IFH>;
-        my $plus   = <$IFH>;
-        my $qual   = <$IFH>; last unless defined $qual;
+        my $header = <IFH>; last unless defined $header;
+        my $seq    = <IFH>;
+        my $plus   = <IFH>;
+        my $qual   = <IFH>; #last unless defined $qual;
 
-        chomp($seq); $n+=4;
+        chomp($seq); 
+        $n+=4;
 
-        next if length($seq)<$k;
+        next if length($seq)<$k; #chack sequence length is less than k
 
         for(my $i=0; $i<=length($seq)-$k; $i++){
             my $kmer = substr($seq,$i,$k);
-            my $prefix = substr($kmer,0,$node_len);
-            my $suffix = substr($kmer,1,$node_len);
+
+            # In De Bruijn graphs:
+            # Parent is the Prefix (length k-1)
+            # Child is the Suffix (length k-1)
+            my $prefix = substr($kmer, 0, $node_len);
+            my $suffix = substr($kmer, 1, $node_len);
 
             $graph->{'P2C'}{$prefix}{$suffix}++;
             $graph->{'C2P'}{$suffix}{$prefix}++;
@@ -118,55 +149,159 @@ sub read_from_input_file {
         }
     }
 
-    close($IFH);
+    close(IFH);
     print STDERR "## Processed $pairs k-mer edges from $n FASTQ lines\n" if $verbose;
 }
 
 sub search_top_to_bottom {
     my ($verbose, $outputfile, $graph, $array) = @_;
-    open(my $OFH, ">", $outputfile) or die "Cannot open $outputfile\n";
+
+    print STDERR "# Writing node list to $outputfile...\n"
+                 if $verbose;
+
+    open(OFH, "> $outputfile") ||
+        die("## ERROR ## Cannot open $outputfile...\n");
 
     my $nc=0;
     foreach my $parent (keys %{$graph}) {
         foreach my $child (keys %{$graph->{$parent}}) {
-            push @$array, $child unless exists $graph->{$child};
-            print $OFH $child,"\n" unless exists $graph->{$child};
-            $nc++;
+            (exists($graph->{$child})) || do {
+                 push @$array, $child;
+                 print OFH $child,"\n";
+                 $nc++;
+            };
+        };
+    };
+    close(OFH);
+    print STDERR "# SAVED $nc terminal nodes\n" if $verbose;
+};
+
+
+sub traverse_graph_debruijn_dfs_collect {
+
+    no warnings 'recursion';
+
+    my ($verbose, $graph, $node, $sequence, $contigs_ref) = @_;
+
+    foreach my $child (keys %{ $graph->{$node} }) {
+
+        # Decrement edge usage counter
+        $graph->{$node}{$child}--;
+
+        print STDERR "DFS: $node -> $child (remaining: $graph->{$node}{$child})\n"
+            if $verbose == 2;
+
+        # Check terminal conditions
+        if (!exists($graph->{$child}) || $graph->{$node}{$child} < 0) {
+
+            my $final_seq = $sequence . substr($child, -1, 1);
+
+            push @$contigs_ref, $final_seq;
+
+            # print STDERR "  TERMINAL -> contig length=", length($final_seq), "\n"
+            #     if $verbose;
+
+        } else {
+
+            my $next_seq = $sequence . substr($child, -1, 1);
+
+            &traverse_graph_debruijn_dfs_collect(
+                $verbose,
+                $graph,
+                $child,
+                $next_seq,
+                $contigs_ref
+            );
         }
     }
-    close($OFH);
-    print STDERR "# SAVED $nc terminal nodes\n" if $verbose;
 }
 
-# ---------------- memoized DFS -----------------
-my %LONGEST_PATH;
-
-sub get_longest_path_from_node {
-    no warnings 'recursion';
-    my ($graph, $node) = @_;
-    return $LONGEST_PATH{$node} if exists $LONGEST_PATH{$node};
-    return $node unless exists $graph->{$node};
-
-    my $best_seq='';
-    foreach my $child (keys %{$graph->{$node}}) {
-        my $child_seq = get_longest_path_from_node($graph, $child);
-        my $merged = $node . substr($child_seq,1);
-        $best_seq = $merged if length($merged)>length($best_seq);
-    }
-    return $LONGEST_PATH{$node} = $best_seq;
-}
 
 sub traverse_graph_function {
-    no warnings 'recursion';
+
     my ($verbose, $outputfile, $graph, $top_parents) = @_;
 
-    open(my $OFH, ">", $outputfile) or die "Cannot open $outputfile\n";
-    my $longest='';
+    print STDERR "# Traversing graph and collecting contigs...\n"
+        if $verbose;
+
+    my @all_contigs = ();
+
+    # ---- DFS for each starting parent ----
     foreach my $parent (@$top_parents) {
-        my $seq = get_longest_path_from_node($graph, $parent);
-        print $OFH $seq,"\n" if $verbose;
-        $longest = $seq if length($seq) > length($longest);
+
+        &traverse_graph_debruijn_dfs_collect(
+            $verbose,
+            $graph,
+            $parent,
+            $parent,      # initial sequence is the whole k-mer
+            \@all_contigs
+        );
     }
-    close($OFH);
-    print STDERR "# Longest sequence length: ", length($longest), "\n";
+
+    print STDERR "# Total contigs generated: ", scalar @all_contigs, "\n"
+        if $verbose;
+
+    # ---- Sort contigs by length descending ----
+    my @sorted = sort { length($b) <=> length($a) } @all_contigs;
+
+    # ---- Keep only 5 longest ----
+    my @top5 = @sorted[0 .. ($#sorted < 4 ? $#sorted : 4)];
+
+    # ---- Output to file ----
+    open(my $OUT, ">", $outputfile)
+        or die "### ERROR: Cannot open $outputfile\n";
+
+    my $count = 1;
+    foreach my $seq (@top5) {
+        my $len = length($seq);
+
+        print $OUT "Sequence $count: $len bases\n";
+        print $OUT "$seq\n\n";
+
+        $count++;
+    }
+
+    close($OUT);
+
+    print STDERR "# Wrote top 5 contigs to $outputfile\n";
 }
+
+
+
+# ---------------- memoized DFS -----------------
+# my %LONGEST_PATH;
+
+# sub get_longest_path_from_node {
+#     no warnings 'recursion';
+#     my ($graph, $node) = @_;
+#     return $LONGEST_PATH{$node} if exists $LONGEST_PATH{$node};
+#     return $node unless exists $graph->{$node};
+
+#     my $best_seq='';
+#     foreach my $child (keys %{$graph->{$node}}) {
+#         my $child_seq = get_longest_path_from_node($graph, $child);
+#         my $merged = $node . substr($child_seq,1);
+#         $best_seq = $merged if length($merged)>length($best_seq);
+#     }
+#     return $LONGEST_PATH{$node} = $best_seq;
+# }
+
+# sub traverse_graph_function {
+
+#     my ($verbose, $outputfile, $graph, $top_parents) = @_;
+    
+#     print STDERR "# Retrieving contigs from graph to $outputfile...\n"
+#           if $verbose;
+
+#     open(CONTIGSFH, ">", $outputfile) ||
+#         die("## ERROR ## Cannot open $outputfile...\n");
+
+#     my $longest='';
+#     foreach my $parent (@$top_parents) {
+#         my $seq = &get_longest_path_from_node($graph, $parent);
+#         print CONTIGSFH $seq,"\n" if $verbose;
+#         $longest = $seq if length($seq) > length($longest);
+#     }
+#     close(CONTIGSFH);
+#     print STDERR "# Longest sequence length: ", length($longest), "\n";
+# }
